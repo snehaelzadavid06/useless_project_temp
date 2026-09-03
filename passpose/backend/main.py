296790cd1c1668309eefd407021850765cc26f6f
snehaelzadavid06 <snehaelzadavid70@gmail.com>
2026-09-04 03:30:20 +0530

    connect FastAPI to password manager

--- passpose/backend/main.py
+++ passpose/backend/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+from .password_manager import save_password, verify_password
+
 
 app = FastAPI()
+
+
+class PasswordSequence(BaseModel):
+    sequence: list[str]
 
 
 @app.get("/")
@@ -14,4 +22,31 @@ def home():
 def health_check():
     return {
         "status": "ok"
+    }
+
+
+@app.post("/password/create")
+def create_password(data: PasswordSequence):
+
+    save_password(data.sequence)
+
+    return {
+        "message": "Password created successfully!"
+    }
+
+
+@app.post("/password/verify")
+def check_password(data: PasswordSequence):
+
+    result = verify_password(data.sequence)
+
+    if result:
+        return {
+            "authenticated": True,
+            "message": "ACCESS GRANTED!"
+        }
+
+    return {
+        "authenticated": False,
+        "message": "ACCESS DENIED!"
     }
